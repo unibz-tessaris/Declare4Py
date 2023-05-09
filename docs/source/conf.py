40b9b826e706d5f8c49ed192a0d9733910f22c42
Ivan Donadello <donadelloivan@gmail.com>
2023-05-09 16:10:44 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,9 @@ release = '2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc', 'nbsphinx']
+#extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc', 'nbsphinx']
+extensions = []
+
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -27,5 +29,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'nature'
 html_static_path = ['_static']
