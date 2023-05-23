264f57b35bbf4f400225d9374010441797a63869
Diellsimeone <61971593+xhedj@users.noreply.github.com>
2023-05-23 12:51:01 +0200

    Changed LTLAnalyzer.py to reflect better the conformance check. Returns a dataframe containing traceid and result of the conformance. Working on using numba engine in the groupby.DataFrameGroupBy.Transform. This requires some changes in the code. Numba Decorators and apply do not seem to be compatible, jitclass doesn't seem to support user-defined types.
    Signed-off-by: xhedj <simeonexhem99@gmail.com>

--- src/Declare4Py/ProcessMiningTasks/ConformanceChecking/LTLAnalyzer.py
+++ src/Declare4Py/ProcessMiningTasks/ConformanceChecking/LTLAnalyzer.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import pdb
 
+import numba
+import numpy
 import pm4py
 
 from src.Declare4Py.D4PyEventLog import D4PyEventLog
@@ -16,6 +18,8 @@ import time
 from joblib import Parallel, delayed
 import multiprocessing
 import swifter
+from numba import njit
+
 """
 Provides basic conformance checking functionalities
 """
@@ -28,6 +32,7 @@ class LTLAnalyzer():
     def __int__(self, ltl_model: LTLModel, data_frame: pandas.DataFrame):
         self.ltl_model: LTLModel = ltl_model
         self.data_frame = data_frame
+
 
     dfa: DFA = None
 
@@ -43,10 +48,11 @@ class LTLAnalyzer():
                 map(lambda x: self.dfa.get_successors(x, temp), current_states),
                 set(),
             )
-        result = any(self.dfa.is_accepting(state) for state in current_states)
-        return result
+        # outcome = any(self.dfa.is_accepting(state) for state in current_states)
+        outcome = [bool(state) if self.dfa.is_accepting(state) == True else False for state in current_states]
+        return outcome[0]
 
-    #Not working
+    # Not working
     def parallel_conf(self, dfgroup, function):
         retLst = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(function)(group) for name, group in dfgroup)
         return pandas.concat(retLst)
@@ -62,24 +68,26 @@ class LTLAnalyzer():
         """
 
         if self.data_frame is None:
-           raise RuntimeError("You must load the log before checking the model.")
+            raise RuntimeError("You must load the log before checking the model.")
         if self.ltl_model is None:
-           raise RuntimeError("You must load the LTL model before checking the model.")
+            raise RuntimeError("You must load the LTL model before checking the model.")
         dfa = ltl2dfa(self.ltl_model.parsed_formula, backend="lydia")
         self.dfa = dfa.minimize()
         group = self.data_frame.groupby('case:concept:name', group_keys=True)
-        self.data_frame['conformance'] = group['concept:name'].transform(self.ltl_conf_check)
 
-        #Not working
-        #self.data_frame['conformance'] = self.parallel_conf(group, self.ltl_conf_check)
+        result = group['concept:name'].apply(self.ltl_conf_check)
 
-        result_df = self.data_frame.sort_values(by=['case:concept:name'])
-        #print(result_df)
-        #for i in range(len(result_df)):
+        result = result.to_frame()
+        print(result)
+        # result = group['concept:name'].transform(self.ltl_conf_check, engine='numba', engine_kwargs = {'nopython': True, 'nogil': False, 'parallel': True})
+
+
+        # Not working
+        # self.data_frame['conformance'] = self.parallel_conf(group, self.ltl_conf_check)
+        # print(result_df)
+        # for i in range(len(result_df)):
         #    print(result_df.loc[i, 'case:concept:name'], result_df.loc[i, 'conformance'])
-        return result_df
-
-
+        return result
 
 
 #df = pm4py.read_xes("/home/xhedj/Desktop/test_logs/InternationalDeclarations.xes")
@@ -88,6 +96,7 @@ class LTLAnalyzer():
 #analyzer = LTLAnalyzer()
 #analyzer.ltl_model = model
 #analyzer.data_frame = df
+#s = time.time()
 #res = analyzer.run()
-
-
+#e = time.time()
+#print(f"Elapsed time: {e - s}")
