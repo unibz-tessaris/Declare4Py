66c1b68f6b1ab1d15dac08c9d9135d0b5d969083
xhedj <simeonexhem99@gmail.com>
2023-09-22 15:52:55 +0200

    Added __init__.py

--- Declare4Py/__init__.py
+++ Declare4Py/__init__.py
@@ -1,6 +1,5 @@
-
 from .Encodings import *
 from .ProcessMiningTasks import *
 from .ProcessModels import *
 from .D4PyEventLog import D4PyEventLog
-import run_log_generator
+from .run_log_generator import *
